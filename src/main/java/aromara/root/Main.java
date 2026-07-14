41e3b31f12dde82cf053944a57b63ae7783d4552
leo <leonidleskovetz@gmail.com>
2026-07-14 11:58:40 +0200

    bump version

--- src/main/java/aromara/root/Main.java
+++ src/main/java/aromara/root/Main.java
@@ -27,7 +27,7 @@ import thaumcraft.api.research.ScanningManager;
 public class Main {
     public static final String MODID = "aromara";
     public static final String NAME = "Aromara Thaumcraft";
-    public static final String VERSION = "1.1.6-BETA";
+    public static final String VERSION = "1.1.7-BETA";
 
     public static SimpleNetworkWrapper packetHandler;
 
